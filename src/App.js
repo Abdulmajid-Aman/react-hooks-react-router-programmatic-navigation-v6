feb9a2102d219dc0b3d260f81c3ec2408ebe7f4e
Abdulmajid Aman Hussein <abdulmajid.aman.student@moringaschool.com>
2024-08-08 17:41:38 +0300

    React programmatic navigation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate, Navigate } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import { useEffect, useState } from "react";
+
 
 function App() {
+  const navigate = useNavigate()
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
 
+  function handleLogin() {
+    setIsLoggedIn(true)
+  }
+
+  function handleLogOut() {
+    setIsLoggedIn(false)
+  }
+
+  useEffect(() => {
+    if(isLoggedIn) {
+      navigate("/")
+    } else {
+      navigate("/login")
+    }
+  }, [isLoggedIn])
 
   return (
     <div className="app">
-      <NavBar />
-      <Outlet />
+      {/* Add conditional rendering so users have to be logged in to see pages on the site */}
+      {isLoggedIn ? <NavBar logout={handleLogOut}  /> : <Navigate to="/login" />}
+      <Outlet context={handleLogin}/>
     </div>
   );
 }
